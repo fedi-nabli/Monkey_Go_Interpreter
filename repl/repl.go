d103ac5c447a24db440d7ba0ad825b945dccb810
fedi-nabli <fedinabli@gmail.com>
2025-12-24 14:51:10 +0100

    Implemented basics of Evaluator and added it to REPL. Evaluator now works with IntegerLiterals

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey_go_interpreter/evaluator"
 	"monkey_go_interpreter/lexer"
 	"monkey_go_interpreter/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
